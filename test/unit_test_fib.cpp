c4af6ec7eedef3ca65f86917c2bc2d751c20d338
Peng Du <rick.peng.du@gmail.com>
2022-12-23 19:32:03 -0800

    add unit test

--- test/unit_test_fib.cpp
+++ test/unit_test_fib.cpp
@@ -3,9 +3,25 @@
 #include "util.h"
 
 TEST(fib, edge_cases) {
-    ASSERT_TRUE(false); // TODO: replace this with real test code
+
+    for (int i = -10; i < 0; i++) {
+        int actual = fib(i);
+
+        ASSERT_EQ(0, actual) << case_string(i);
+    }
 }
 
 TEST(fib, normal_cases) {
-    ASSERT_TRUE(false); // TODO: replace this with real test code
+    int expect[] = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55,
+                    89, 144, 233, 377, 610, 987, 1597, 2584,
+                    4181, 6765, 10946, 17711, 28657, 46368, 75025,
+                    121393, 196418, 317811};
+
+    int size = sizeof(expect) / sizeof(expect[0]);
+
+    for (int i = 0; i < size; i++) {
+        int actual = fib(i);
+
+        ASSERT_EQ(expect[i], actual) << case_string(i);
+    }
 }
