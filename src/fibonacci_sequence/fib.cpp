035c85d943f46a8b582be60c867fccebde954a39
Peng Du <rick.peng.du@gmail.com>
2022-12-23 19:38:34 -0800

    finish fib

--- src/fibonacci_sequence/fib.cpp
+++ src/fibonacci_sequence/fib.cpp
@@ -1,3 +1,22 @@
 int fib(int n) {
-    return -1; // TODO: replace this placeholder with your real code
+    if (n <= 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+
+    int *data = new int[n + 1]; // why +1?
+    data[0] = 0;
+    data[1] = 1;
+
+    for (int i = 2; i <= n; ++i) {
+        data[i] = data[i - 1] + data[i - 2];
+    }
+
+    int ret = data[n];
+
+    delete[]data;   // make sure new'd memory is deleted to prevent memory leak
+
+    return ret;
 }
